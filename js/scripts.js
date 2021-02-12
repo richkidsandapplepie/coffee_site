8b25cb529322877099826eaaaabf0182165c6174
Richard Kwon <richardhkwon@gmail.com>
2021-02-11 23:02:55 -0600

    order scripts

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(function() {
+
+  $("#next-button").click(
+    function() {
+      $("#next-button").hide();
+      $("#cart-container").show("slow");
+      $(".checkout-form-container").show("slow");
+    
+    }
+  );
+
+
+});
+
+
